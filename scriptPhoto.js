7105e5a595fe70644afe84749a35258aa420c321
akanna1218 <ajithkannapandian@gmail.com>
2024-04-25 17:46:50 +0530

    fixed some feature for mobile view

--- scriptPhoto.js
+++ scriptPhoto.js
@@ -2,18 +2,19 @@ const photo=document.querySelectorAll('.photos');  // queryselector for getting 
 const mainImage=document.querySelector('.hero-section');
 const closeButton=document.querySelector('.close-image');
 const popImage=document.querySelector('.pop-image');
-const mainContainer=document.querySelector('.container')
+const mainContainer=document.querySelector('.container');
+const pops=document.querySelector('.pop');
 
 
 photo.forEach((i)=>{                                // if you are giving same class name to bunch of tags , you need to iterate them to access individual class name
 i.addEventListener('click',function imageChanger(){
     
-    popImage.style.background=`url(${i.src}) center/cover no-repeat`;
-    popImage.style.display='block';
+    // popImage.style.background=`url(${i.src}) center/cover no-repeat`;
+    popImage.style.display='flex';
     mainContainer.style.filter='blur(5px)';
     popImage.style.filter='blur(0px)';
     document.body.style.overflow='hidden';
-
+    pops.src=`${i.src}`
 }
 )});
 
